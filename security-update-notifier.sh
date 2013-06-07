8f9ee424462bb62a9c400795853eda5775aa7050
Callum Macdonald <git@callum-macdonald.com>
2013-06-07 13:07:49 +0200

    Initial commit.

---
+++ security-update-notifier.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Grab the pending updates like 4;2
+updates=$(/usr/lib/update-notifier/apt-check 2>&1)
+
+# If there are no pending updates, exit now
+if [ $updates == "0;0" ]
+then
+	exit
+fi
+
+# Get the number of security updates (after the colon)
+security=$(echo "$updates" | cut -d ";" -f 2)
+
+if [ $security != "0" ]
+then
+	# Generate some output which will be piped to cron
+	/usr/lib/update-notifier/apt-check --human-readable
+fi
